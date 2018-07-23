e77f8bae9202fc111f30564c900256381adfa03a
Anod Chow <anod@jidu-health.com>
2018-07-23 17:53:09 +0800

    rfid module for nodemcu<unfinished>

---
+++ app/module/rfid.c
@@ -0,0 +1,19 @@
+#include "module.h"
+#include "c_stdlib.h"
+#include "platform.h"
+#include "lauxlib.h"
+#include "rfid.h"
+
+static const LUA_REG_TYPE rfid_map[] = {
+  // for module function
+  { LSTRKEY("setup"),  },
+  { LSTRKEY("request"),  },
+  { LSTRKEY("anticoll"), },
+  { LSTRKEY("select"), },
+  { LSTRKEY("auth"), },
+  { LSTRKEY("read"), },
+  // for module constant
+  { LNILKEY, LNILKEY }
+};
+
+NODEMCU_MODULE( RFID, "rfid", rfid_map, NULL );
---
+++ app/rfid/rfid.h
@@ -0,0 +1,307 @@
+#ifndef __RFID_H__
+#define __RFID_H__
+
+#include "c_stdint.h"
+
+#ifdef __cplusplus
+//extern "C"{
+#endif
+
+// see: https://github.com/ondryaso/pi-rc522
+
+// page0: for command and status
+enum {
+  CommandReg = 1,
+  CommIEnReg,
+  DivIEnReg,
+  CommIrqReg,
+  DivIrqReq,
+  ErrorReg,
+  Status1Reg,
+  Status2Reg,
+  FIFODataReg,
+  FIFOLevelReg,
+  WaterLevelReg,
+  ControlReg,
+  BitFramingReg,
+  CollReg
+};
+
+// page1: register for command
+enum{
+  ModeReg = 1,
+  TxModeReg,
+  RxModeReg,
+  TxControlReg,
+  TxAutoReg,
+  TxSelReg,
+  RxSelReg,
+  RxThresholdReg,
+  DemodReg,
+  MifareReg = 0xc,
+  SerialSpeedReg = 0xf
+};
+
+// page2: register for CFG
+enum{
+  CRCResultRegL = 1,
+  CRCResultRegH,
+  ModWidthReg = 4,
+  RfCfgReg = 6,
+  GsNReg,
+  CWGsPReg,
+  ModGsPReg,
+  TModeReg,
+  TPrescalerReg,
+  TReloadRegL,
+  TReloadRegH,
+  TCounterValueRegL,
+  TCounterValueRegH
+};
+
+// page3: register for test
+enum{
+  TestSel1Reg = 1,
+  TestSel2Reg,
+  TestPinEnReg,
+  TestPinValueReg,
+  TestBusReg,
+  AutoTestReg,
+  VersionReg,
+  AnalogTestReg,
+  TestDAC1Reg,
+  TestDAC2Reg,
+  TestADCReg
+};
+
+typedef uint8_t u8;
+/* CommandReg */
+typedef struct {
+  u8 Command:4;
+  u8 PowerDown:1, RcvOff:1;
+} bCommandReg;
+/* CommIEnReg */
+typedef struct{
+  u8 TimerIEn:1, ErrIEn:1, LoAlertIEn:1, HiAlertIEn:1;
+  u8 IdleEn:1, RxIEn:1, TxIEn:1, IRqInv:1;
+} bCommIEnReg;
+/* DivIEnReg */
+typedef struct {
+u8 :2, CRCIEn:1, :1;
+  u8 SigninActIEn:1, :2, IRQPushPull:1;
+} bDivEnReg;
+/* CommIRqReg */
+typedef struct {
+  u8 TimerIRq:1, ErrIRq:1, LoAlertIRq:1, HiAlertIRq:1;
+  u8 IdleIRq:1, RxIRq:1, TxIRq:1, Set1:1;
+} bCommIRqReg;
+/* DivIRqReq */
+typedef struct {
+u8 :2, CRCIRq:1, :1;
+  u8 SigninActIRq:1, :2, Set2:1;
+} bDivIRqReq;
+/* ErrorReg */
+typedef struct {
+  u8 ProtocolErr:1, ParityErr:1, CRCErr:1, CollErr:1;
+  u8 BufferOvfl:1, :1, TempErr:1, WrErr:1;
+} bErrorReg;
+/* Status1Reg */
+typedef struct {
+  u8 LoAlert:1, HiAlert:1, :1, TRunning:1;
+  u8 IRq:1, CRCReady:1, CRCOk:1;
+} bStatus1Reg;
+/* Status2Reg */
+typedef struct {
+  u8 ModemState:3, MFCrypto1On:1;
+  u8 :2, I2CforceHS:1, TempSensOff:1;
+} bStatus2Reg;
+/* FIFODataReg */
+typedef struct {
+  u8 FIFOData;
+} bFIFODataReg;
+/* FIFOLevelReg */
+typedef struct {
+  u8 FIFOLevel:7, FlushBuffer:1;
+} bFIFOLevelReg;
+/* WaterLevelReg */
+typedef struct {
+  u8 WaterLevel:6;
+} bWaterLevelReg;
+/* ControlReg */
+typedef struct {
+  u8 RxLastBits:3, :1;
+  u8 BitAlways1:1, :1, TStartNow:1, TStopNow:1;
+} bControlReg;
+/* BitFramingReg */
+typedef struct {
+  u8 TxLastBits:3, :1;
+  u8 RxAlign:3, StartSend:1;
+} bBitFramingReg;
+/* CollReg */
+typedef struct {
+  u8 CollPos:5, CollPosNotValid:1, :1, ValuesAfterColl:1;
+} bCollReg;
+/* ModeReg */
+typedef struct {
+  u8 CRCPreset:2, :1, PolSignin:1;
+u8 :1, TxWaitRF:1;
+} bModeReg;
+/* TxModeReg */
+typedef struct {
+u8 :3, InvMode:1;
+  u8 TxSpeed:3, TxCRCEn:1;
+} bTxModeReg;
+/* RxModeReg */
+typedef struct {
+u8 :3, RxNoErr:1;
+  u8 RxSpeed:3, RxCRCEn:1;
+} bRxModeReg;
+/* TxControlReg */
+typedef struct {
+  u8 Tx1RFEn:1, Tx2RFEn:1, :1, Tx2CW:1;
+  u8 InvTx1RFOff:1, InvTx2RFOff:1, InvTx1RFOn:1, InvTx2RFOn:1;
+} bTxControlReg;
+/* TxSelReg */
+typedef struct {
+  u8 SigOutSel:4;
+  u8 DriverSel:2;
+} bTxSelReg;
+/* RxSelReg */
+typedef struct {
+  u8 RxWait: 6, UartSel:2;
+} bRxSelReg;
+/* RxThresholdReg */
+typedef struct {
+  u8 CollLevel:3, :1;
+  u8 MinLevel:4;
+} bRxThresholdReg;
+/* DemodReg */
+typedef struct {
+  u8 TauSync:2, TauRcv:2;
+u8 :1, FixIQ:1, AddIQ:2;
+} bDemodReg;
+/* SerialSpeedReg */
+typedef struct {
+  u8 BR_T1:5, BR_T0:3;
+} bSerialSpeedReg;
+/* CRCResultRegH, CRCResultRegL */
+typedef struct {
+  u8 CRCResultLSB;
+  u8 CRCResultMSB;
+} wCRCResultReg;
+/* ModWidthReg */
+typedef struct {
+  u8 ModWidth;
+} bModWidthReg;
+/* RFCfgReg */
+typedef struct {
+  u8 BitAlways1:4, RxGain:3;
+} bRFCfgReg;
+/* GsNReg */
+typedef struct {
+  u8 ModGsN:4, CWGsN:4;
+} bGsNReg;
+/* CWGsPReg */
+typedef struct {
+  u8 CWGsP:6;
+} bCWGsPReg;
+/* ModGsPReg */
+typedef struct {
+  u8 ModGsP:6;
+} bModGsPReg;
+/* TModeReg */
+typedef struct {
+  u8 TPrescaler_Hi:4;
+  u8 TAutoRestart:1, TGated:2, TAuto:1;
+} bTModeReg;
+/* TPrescalerReg */
+typedef struct {
+  u8 TPrescaler_Lo;
+} bTPrescalerReg;
+/* TReloadRegH, TReloadRegL */
+typedef struct {
+  u8 TReloadVal_Lo;
+  u8 TReloadVal_Hi;
+} wTReloadReg;
+/* TCounterValRegH, TCounterValRegL */
+typedef struct {
+  u8 TCounterVal_Lo;
+  u8 TCounterVal_Hi;
+} wTCounterValReg;
+/* TestSel1Reg */
+typedef struct {
+  u8 TstBusBitSel:3;
+} bTestSel1Reg;
+/* TestSel2Reg */
+typedef struct {
+  u8 TestBusSel:5, PRBS15:1, PRBS9:1, TstBusFlip:1;
+} bTestSel2Reg;
+/* TestPinEnReg */
+typedef struct {
+  u8 TestPinEn:7;
+} bTestPinEnReg;
+/* TestPinValueReg */
+typedef struct {
+  u8 TestPinValue:7, UseIO:1;
+} bTestPinValueReg;
+/* TestBusReg */
+typedef struct {
+  u8 TestBus;
+} bTestBusReg;
+/* AutoTestReg */
+typedef struct {
+  u8 SelfTest:4, :2, BitAlways1:1;
+} bAutoTestReg;
+/* VersionReg */
+typedef struct {
+  u8 Version;
+} bVersionReg;
+/* AnalogTestReg */
+typedef struct {
+  u8 AnalogSelAux2:4;
+  u8 AnalogSelAux1:4;
+} bAnalogTestReg;
+/* TestDAC1Reg */
+typedef struct {
+  u8 TestDAC1:6;
+} bTestDAC1Reg;
+/* TestDAC2Reg */
+typedef struct {
+  u8 TestDAC2:6;
+} bTestDAC2Reg;
+/* TestADCReg */
+typedef struct {
+  u8 ADC_Q:4;
+  u8 ADC_I:4;
+} bTestADCReg;
+
+// Command Set
+#define CMD_IDLE               0
+#define CMD_MEM                1
+#define CMD_GENERATE_RANDOM_ID 2
+#define CMD_CALC_CRC           3
+#define CMD_TRANSMIT           4
+#define CMD_NO_CMD_CHANGE      7
+#define CMD_RECEIVE            8
+#define CMD_TRANSCEIVE         12
+#define CMD_MFAUTHENT          14
+#define CMD_SOFT_RESET         15
+
+int rfid_init();
+int rfid_request();
+int rfid_anticoll();
+int rfid_getcrc();
+int rfid_select();
+int rfid_auth();
+
+int rfid_halt();
+int rfid_read();
+int rfid_write();
+int rfid_restart();
+
+  
+#ifdef __cplusplus
+//}
+#endif
+
+#endif//__RFID_H__
